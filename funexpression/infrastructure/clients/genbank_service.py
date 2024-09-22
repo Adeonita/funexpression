6df49e931b68ca18de0c6849834512e8847cabae
adeonita <adeonita.sousa@gmail.com>
2024-09-22 17:38:18 -0300

    chore: fix infrestructure folder name

--- funexpression/infrastructure/clients/genbank_service.py
+++ funexpression/infrastructure/clients/genbank_service.py
@@ -5,28 +5,11 @@ import urllib.request
 from ftplib import FTP
 from urllib.parse import urlparse
 
-from Bio import Entrez, SeqIO
-from dotenv import load_dotenv
+from Bio import Entrez
 
-load_dotenv(override=True)
-
-mail = ''
-Entrez.email = mail
+Entrez.email = os.getenv('ENTREZ_EMAIL')
 
 def _ftp_file_exists(ftp_url: str, file_name: str):
-    """
-    The function `check_ftp_file_existence` checks if a file exists on an FTP
-    server given the FTP URL and file name.
-
-    :param ftp_url: The `ftp_url` parameter is a string that represents the URL of
-    the FTP server. It should follow the format
-    `ftp://<server_address>/<file_path>`, where `<server_address>` is the address
-    of the FTP server and `<file_path>` is the path to the directory where the file
-    :param file_name: The name of the file you want to check for existence on the
-    FTP server
-    :return: a boolean value indicating whether the specified file exists on the
-    FTP server.
-    """
     url_parts = urlparse(ftp_url)
     server_address = url_parts.netloc
     file_path = url_parts.path
@@ -106,22 +89,6 @@ def _get_ftp_url(summary):
     return ftp_url
 
 def download_fasta_sequence_by_id(acession_number: str):
-    """
-    The function `download_fasta_sequence_by_id` downloads a FASTA sequence file
-    from an FTP server using an accession number and returns the file path,
-    organism name, and any errors encountered.
-
-    :param acession_number: The `acession_number` parameter is a string that
-    represents the accession number of a genome assembly. It is used to search for
-    and download the FASTA sequence file for that specific assembly
-    :return: The function `download_fasta_sequence_by_id` returns three values:
-    1. `fasta_output_path`: The path to the downloaded FASTA file.
-    2. `full_organism_name`: The full name of the organism associated with the
-    accession number.
-    3. `False`: A boolean value indicating whether the download was successful or
-    not.
-    """
-
     try:
         assembly_id = _get_id_by_acession_number(acession_number, database='assembly')
 
