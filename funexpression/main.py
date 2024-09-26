34228e23f7533702e856994507af02e6a094aff6
adeonita <adeonita.sousa@gmail.com>
2024-09-26 08:34:29 -0300

    chore: remove comment code

--- funexpression/main.py
+++ funexpression/main.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 from fastapi import APIRouter, FastAPI
 from application.interfaces.expression_request_payload import ExpressionCalculateRequest
-# from infrastructure.clients.genbank_service import GenBankService
+from infrastructure.clients.genbank_service import GenBankService
 from infrastructure.clients.geo_service import GEOService
 
 load_dotenv('../.env')
@@ -11,10 +11,10 @@ router = APIRouter()
 
 @router.post("/expression/calculate/")
 def expression_calculate(request: ExpressionCalculateRequest):
-    # gene_id = request.reference_genome_acession_number
+    gene_id = request.reference_genome_acession_number
     transcriptome = request.control_organism.srr_acession_number_1
     GEOService().get_fasta_sequence(transcriptome)
-    # genbank = GenBankService().download_fasta_sequence_by_id(gene_id)
+    GenBankService().download_fasta_sequence_by_id(gene_id)
     return request
 
 app.include_router(router)
