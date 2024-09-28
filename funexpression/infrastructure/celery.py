4912754c6fcbb62616c7679e77ab5605db0efcb4
Adeonita dos Santos <40674971+Adeonita@users.noreply.github.com>
2024-09-28 16:04:10 -0300

    Feat/add celery (#3)
    * minor fixx: fix file name
    
    * chore: get necessary values to call task
    
    * feat: create method to get srr list
    
    * feat: add and config celery
    
    * feat: add celery and rabbit mq to container
    
    * feat: add celery to pyptoject.toml

---
+++ funexpression/infrastructure/celery.py
@@ -0,0 +1,9 @@
+from celery import Celery
+
+app = Celery(
+        'geo',
+        broker='pyamqp://admin:pass@rabbitmq:5672//',
+        include=[
+            'infrastructure.clients.geo_service'
+        ]
+    )
