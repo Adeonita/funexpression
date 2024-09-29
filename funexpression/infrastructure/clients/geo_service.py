6e00a01c31c312876dbbba90d15aa230c8c1271d
adeonita <adeonita.sousa@gmail.com>
2024-09-29 19:51:16 -0300

    refactor: change code to a class

--- funexpression/infrastructure/clients/geo_service.py
+++ funexpression/infrastructure/clients/geo_service.py
@@ -1,15 +1,17 @@
-from infrastructure.celery import app
+import logging
+
 from infrastructure.clients.commands import create_folder_if_not_exist, fasterq_dump, prefetch, remove_trash
 
+class GEOService:
 
-@app.task
-def get_fasta_sequence(run_id, group, sra_id):
-    try:
-        prefetch(sra_id)
-        outdir = create_folder_if_not_exist(run_id, group, sra_id)
-        fasterq_dump(sra_id, outdir)
-        remove_trash(sra_id)
+    def get_fasta_sequence_from_ncbi(self, run_id, group, sra_id):
+        logging.info("Starting download fastq sequence from ncbi ")
 
-    except Exception as e:
-        return f"there was an error when downloading fasta sequence {e}"
-    
+        try:
+            prefetch(sra_id)
+            outdir = create_folder_if_not_exist(run_id, group, sra_id)
+            fasterq_dump(sra_id, outdir)
+            remove_trash(sra_id)
+
+        except Exception as e:
+            return f"there was an error when downloading fasta sequence {e}"
