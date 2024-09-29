d8c481c9fae5ca6aae8aa9bb2d21604195238689
adeonita <adeonita.sousa@gmail.com>
2024-09-29 19:03:06 -0300

    refactor: refactor geo_service

--- funexpression/infrastructure/clients/geo_service.py
+++ funexpression/infrastructure/clients/geo_service.py
@@ -1,24 +1,15 @@
-import os
-import subprocess
-
 from infrastructure.celery import app
+from infrastructure.clients.commands import create_folder_if_not_exist, fasterq_dump, prefetch, remove_trash
 
-def _create_folder_if_not_exist(run_id, directory_name: str, acession_number:str):
-    temp_transcriptome = os.path.join(
-        os.getcwd(), run_id, directory_name, acession_number
-    )
-
-    if not os.path.exists(temp_transcriptome):
-        os.makedirs(temp_transcriptome)
-    
-    return temp_transcriptome
 
 @app.task
 def get_fasta_sequence(run_id, group, sra_id):
+    try:
+        prefetch(sra_id)
+        outdir = create_folder_if_not_exist(run_id, group, sra_id)
+        fasterq_dump(sra_id, outdir)
+        remove_trash(sra_id)
 
-    print(f"Generating fastq for: {sra_id}")
-    outdir = _create_folder_if_not_exist(run_id, group, sra_id)
-    fastq_dump = f"fastq-dump --outdir {outdir} {sra_id}"
-
-    print(f"The command used was: {fastq_dump} ")
-    subprocess.call(fastq_dump, shell=True)
+    except Exception as e:
+        return f"there was an error when downloading fasta sequence {e}"
+    
