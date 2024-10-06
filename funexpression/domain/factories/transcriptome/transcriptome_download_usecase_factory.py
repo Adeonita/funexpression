a268c68478b77c4531310ede7b209363345281ed
adeonita <adeonita.sousa@gmail.com>
2024-10-05 23:58:52 -0300

    chore: replace in  transcriptome factory

--- funexpression/domain/factories/transcriptome/transcriptome_download_usecase_factory.py
+++ funexpression/domain/factories/transcriptome/transcriptome_download_usecase_factory.py
@@ -2,7 +2,10 @@ from domain.usecases.transcriptome.transcriptome_download_usecase import (
     TranscriptomeDownloadUseCase,
 )
 from infrastructure.bio_database.geo_adapter import GeoAdapter
-from infrastructure.repositories.pipeline_repository import PipelineRepository
+
+from infrastructure.repositories.pipeline_repository_mongo import (
+    PipelineRepositoryMongo,
+)
 
 
 class TranscriptomeDownloadUseCaseFactory:
@@ -10,6 +13,6 @@ class TranscriptomeDownloadUseCaseFactory:
     @staticmethod
     def create():
         geo_adapter = GeoAdapter()
-        pipeline_repository = PipelineRepository()
+        pipeline_repository = PipelineRepositoryMongo()
 
         return TranscriptomeDownloadUseCase(geo_adapter, pipeline_repository)
