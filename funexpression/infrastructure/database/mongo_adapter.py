e6e31b4300c802d37daf7c45c1adf4f4e655e868
adeonita <adeonita.sousa@gmail.com>
2024-10-06 00:34:30 -0300

    feat: add mongo adapter

--- funexpression/infrastructure/database/mongo_adapter.py
+++ funexpression/infrastructure/database/mongo_adapter.py
@@ -1,27 +1,56 @@
+from bson import ObjectId
+from pymongo import MongoClient
+
+
 class MongoAdapter:
 
-    def __init__(self, mongo_client):
-        self.client = mongo_client
+    client = None
 
-    def get_collection(self, collection_name):
-        return self.client[collection_name]
+    def __init__(self, host="mongo", port=27017, user="root", password="example"):
+        self.client = MongoClient(f"mongodb://{user}:{password}@{host}:{port}/")
+
+    def _database_connection(self, database_name="funexpression"):
+        return self.client[database_name]
+
+    def _get_collection(self, collection_name):
+        db_conn = self._database_connection()
+
+        return db_conn.get_collection(collection_name)
 
     def get_all(self, collection_name):
-        collection = self.get_collection(collection_name)
+        collection = self._get_collection(collection_name)
+
         return collection.find()
 
     def get_by_id(self, collection_name, id):
-        collection = self.get_collection(collection_name)
+        collection = self._get_collection(collection_name)
+
         return collection.find_one({"_id": ObjectId(id)})
 
-    def insert(self, collection_name, data):
-        collection = self.get_collection(collection_name)
-        return collection.insert_one(data)
+    def create(self, collection_name, data):
+        collection = self._get_collection(collection_name)
+        result = collection.insert_one(data)
+
+        return result.inserted_id
+
+    def find(self, collection_name, query):
+        collection = self._get_collection(collection_name)
+
+        return collection.find(query).to_list()
 
     def update(self, collection_name, id, data):
-        collection = self.get_collection(collection_name)
+        collection = self._get_collection(collection_name)
         return collection.update_one({"_id": ObjectId(id)}, {"$set": data})
 
     def delete(self, collection_name, id):
-        collection = self.get_collection(collection_name)
+        collection = self._get_collection(collection_name)
         return collection.delete_one({"_id": ObjectId(id)})
+
+    def updateById(self, pipeline_id, object):
+        collection = self._get_collection("pipelines")
+
+        updated_collection = collection.update_one(
+            {"_id": ObjectId(pipeline_id)}, {"$set": object}
+        )
+
+        return updated_collection.upserted_id
