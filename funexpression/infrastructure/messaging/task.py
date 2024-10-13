3e18e5f8245c74f386eec449fa2cee73da5ac777
adeonita <adeonita.sousa@gmail.com>
2024-10-12 22:17:59 -0300

    feat: add organism group to sra_transcriptome_download

--- funexpression/infrastructure/messaging/task.py
+++ funexpression/infrastructure/messaging/task.py
@@ -1,3 +1,4 @@
+from domain.entities.triplicate import OrganinsGroupEnum
 from domain.factories.transcriptome.conversion_sra_to_fasta_usecase_factory import (
     ConversionSraToFastaUseCaseFactory,
 )
@@ -5,6 +6,9 @@ from domain.factories.transcriptome.transcriptome_download_usecase_factory impor
     TranscriptomeDownloadUseCaseFactory,
 )
 from domain.usecases.base_usecase import BaseUseCase
+from domain.usecases.conversion.input.conversion_sra_to_fasta_usecase_input import (
+    ConversionSraToFastaUseCaseInput,
+)
 from domain.usecases.transcriptome.input.transcriptome_download_usecase_input import (
     TranscriptomeDownloadUseCaseInput,
 )
@@ -15,7 +19,9 @@ from ports.infrastructure.messaging.task_port import TaskPort
 class Task(TaskPort):
 
     @app.task(bind=True, queue="geo_sra_download")
-    def sra_transcriptome_download(self, sra_id: str, pipeline_id: str):
+    def sra_transcriptome_download(
+        self, sra_id: str, pipeline_id: str, organism_group: str
+    ):
 
         print(f"Aqui é o sra_id: {sra_id}")
         print(f"Aqui é o pipeline_id: {pipeline_id}")
@@ -26,19 +32,16 @@ class Task(TaskPort):
         #     )
 
         #     input = TranscriptomeDownloadUseCaseInput(
-        #         sra_id=sra_id, pipeline_id=pipeline_id
+        #         sra_id=sra_id, pipeline_id=pipeline_id, organism_group=organism_group
         #     )
         #     transcriptome_download_usecase.execute(input)
         # except Exception as e:
         #     return f"there was an error when downloading sra sequence {e}"
 
     @app.task(bind=True, queue="sra_to_fasta_conversion")
-    def sra_to_fasta_conversion(self, pipeline_id: str):
+    def sra_to_fasta_conversion(self, sra_id, pipeline_id, organism_group):
         try:
             conversion_usecase = ConversionSraToFastaUseCaseFactory.create()
-            conversion_usecase.execute(pipeline_id)
+            conversion_usecase.execute(sra_id, pipeline_id, organism_group)
         except Exception as e:
             return f"there was an error when downloading sra sequence {e}"
-
-
-# celery -A infrastructure.messaging.task worker -l info --pool=threads --queues=geo_sra_download
