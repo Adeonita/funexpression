4f119230c71f1cef4f8d3fe8a9ebcf56a3f8fc07
adeonita <adeonita.sousa@gmail.com>
2024-10-26 12:22:28 -0300

    feat: add deprecated function

--- funexpression/application/helpers/helper.py
+++ funexpression/application/helpers/helper.py
@@ -1,4 +1,6 @@
 from application.interfaces.expression_request_payload import Triplicate
+import warnings
+import functools
 
 
 def get_srr_list(triplicate: Triplicate):
@@ -11,3 +13,22 @@ def get_srr_list(triplicate: Triplicate):
 
 def get_user_name_by_email(email: str):
     return email.split(".")[0]
+
+
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.simplefilter("always", DeprecationWarning)  # turn off filter
+        warnings.warn(
+            "Call to deprecated function {}.".format(func.__name__),
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        warnings.simplefilter("default", DeprecationWarning)  # reset filter
+        return func(*args, **kwargs)
+
+    return new_func
