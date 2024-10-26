3b01dfe37713f83f0fa14622b1a67cdbc2a32178
adeonita <adeonita.sousa@gmail.com>
2024-10-26 14:43:52 -0300

    feat: add more mothods to storage path ports

--- funexpression/ports/infrastructure/storage/storage_path_port.py
+++ funexpression/ports/infrastructure/storage/storage_path_port.py
@@ -19,10 +19,16 @@ class StoragePathsPort(Protocol):
     def get_genome_paths(self, genome_id) -> GenomePaths:
         pass
 
+    def get_converting_paths(self, pipeline_id, organism_group, sra_id) -> Paths:
+        pass
+
     def get_trimming_paths(self, pipeline_id, organism_group, sra_id) -> Paths:
         pass
 
     def get_aligner_path(
         self, pipeline_id: str, organism_group: str, sra_id: str
     ) -> Paths:
+        pass
+
+    def _create_outdir_if_not_exist(self, pipeline_id: str, step: str, group: str):
         pass
