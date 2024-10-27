93f8474d23c39e688a711ffe58f9be847bafb444
adeonita <adeonita.sousa@gmail.com>
2024-10-26 22:52:42 -0300

    feat: inject storage_paths as dependencei from PipelineCreateUseCase

--- funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
+++ funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
@@ -3,12 +3,16 @@ from infrastructure.messaging.task import Task
 from infrastructure.repositories.pipeline_repository_mongo import (
     PipelineRepositoryMongo,
 )
+from infrastructure.storage.storage_path_adapter import StoragePathsAdapter
 
 
 class PipelineCreateUseCaseFactory:
 
     @staticmethod
     def create():
+        storage_paths = StoragePathsAdapter()
         pipeline_repository = PipelineRepositoryMongo()
 
-        return PipelineCreateUseCase(pipeline_repository=pipeline_repository)
+        return PipelineCreateUseCase(
+            pipeline_repository=pipeline_repository, storage_paths=storage_paths
+        )
