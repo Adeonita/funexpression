ac84beeb32cf8f85ec39cb95d26fa67813e37f29
Adeonita dos Santos <40674971+Adeonita@users.noreply.github.com>
2024-11-02 16:13:14 -0300

    feat: add TranscriptomeDownloadTask

---
+++ funexpression/domain/tasks/transcriptome_download_task.py
@@ -0,0 +1,28 @@
+from domain.entities.pipeline import Pipeline
+from domain.entities.triplicate import OrganinsGroupEnum, SRAFile, Triplicate
+from infrastructure.celery import download_sra_task
+
+
+class TranscriptomeDownloadTask:
+    def _download_transcriptomes(self, pipeline: Pipeline):
+        self._download_triplicate(
+            pipeline.control_organism, pipeline.id, OrganinsGroupEnum.CONTROL
+        )
+
+        self._download_triplicate(
+            pipeline.experiment_organism, pipeline.id, OrganinsGroupEnum.EXPERIMENT
+        )
+
+    def _download_triplicate(
+        self, tri: Triplicate, pipeline_id: str, organism_group: OrganinsGroupEnum
+    ):
+        self._download_sra(tri.srr_1, pipeline_id, organism_group)
+        self._download_sra(tri.srr_2, pipeline_id, organism_group)
+        self._download_sra(tri.srr_3, pipeline_id, organism_group)
+
+    def _download_sra(
+        self, sra_file: SRAFile, pipeline_id: str, organism_group: OrganinsGroupEnum
+    ):
+        sra_id = sra_file.acession_number
+
+        download_sra_task(sra_id, pipeline_id, organism_group)
