377d0521114f577fec9fdf700bb0f0e532cace3b
adeonita <adeonita.sousa@gmail.com>
2024-11-02 13:42:52 -0300

    refactor: remove logic to handle with status from pipeline create usecase

--- funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
+++ funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
@@ -1,4 +1,5 @@
 from domain.usecases.pipeline.pipeline_create_usecase import PipelineCreateUseCase
+from domain.usecases.pipeline.pipeline_gateway import PipelineGateway
 from infrastructure.messaging.task import Task
 from infrastructure.repositories.pipeline_repository_mongo import (
     PipelineRepositoryMongo,
@@ -12,7 +13,10 @@ class PipelineCreateUseCaseFactory:
     def create():
         storage_paths = StoragePathsAdapter()
         pipeline_repository = PipelineRepositoryMongo()
+        pipeline_gateway = PipelineGateway(pipeline_repository=pipeline_repository)
 
         return PipelineCreateUseCase(
-            pipeline_repository=pipeline_repository, storage_paths=storage_paths
+            pipeline_repository=pipeline_repository,
+            storage_paths=storage_paths,
+            pipeline_gateway=pipeline_gateway,
         )
