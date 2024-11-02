54a20de2c2251bad4911036e0e34544569265e69
adeonita <adeonita.sousa@gmail.com>
2024-11-02 18:33:18 -0300

    refactor: inject pipeline_task as dependency to PipelineGateway

--- funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
+++ funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
@@ -1,3 +1,5 @@
+from domain.tasks.pipeline_task import PipelineTask
+from domain.tasks.transcriptome.transcriptome_align_task import TranscripomeAlignTask
 from domain.usecases.pipeline.pipeline_create_usecase import PipelineCreateUseCase
 from domain.usecases.pipeline.pipeline_gateway import PipelineGateway
 from infrastructure.messaging.task import Task
@@ -13,7 +15,11 @@ class PipelineCreateUseCaseFactory:
     def create():
         storage_paths = StoragePathsAdapter()
         pipeline_repository = PipelineRepositoryMongo()
-        pipeline_gateway = PipelineGateway(pipeline_repository=pipeline_repository)
+        transcriptome_align_task = TranscripomeAlignTask(storage_paths=storage_paths)
+        pipeline_task = PipelineTask(transcriptome_align_task)
+        pipeline_gateway = PipelineGateway(
+            pipeline_repository=pipeline_repository, pipeline_task=pipeline_task
+        )
 
         return PipelineCreateUseCase(
             pipeline_repository=pipeline_repository,
