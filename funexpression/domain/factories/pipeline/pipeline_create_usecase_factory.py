75b0d7ce02775590c7e688e05a746fe2751ea3fc
Adeonita <adeonita.sousa@gmail.com>
2024-11-05 01:04:52 -0300

    feat: update PipelineTask and add more parameters in constructor

--- funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
+++ funexpression/domain/factories/pipeline/pipeline_create_usecase_factory.py
@@ -1,5 +1,9 @@
+from domain.tasks.genome.genome_download_task import GenomeDownloadTask
 from domain.tasks.pipeline_task import PipelineTask
 from domain.tasks.transcriptome.transcriptome_align_task import TranscripomeAlignTask
+from domain.tasks.transcriptome.transcriptome_convert_task import TranscripomeConvertTask
+from domain.tasks.transcriptome.transcriptome_download_task import TranscriptomeDownloadTask
+from domain.tasks.transcriptome.transcriptome_trim import TranscriptomeTrimTask
 from domain.usecases.pipeline.pipeline_create_usecase import PipelineCreateUseCase
 from domain.usecases.pipeline.pipeline_gateway import PipelineGateway
 from infrastructure.messaging.task import Task
@@ -16,7 +20,12 @@ class PipelineCreateUseCaseFactory:
         storage_paths = StoragePathsAdapter()
         pipeline_repository = PipelineRepositoryMongo()
         transcriptome_align_task = TranscripomeAlignTask(storage_paths=storage_paths)
-        pipeline_task = PipelineTask(transcriptome_align_task)
+        genome_download_task = GenomeDownloadTask()
+        transcriptome_download_task = TranscriptomeDownloadTask()
+        transcriptome_convert_task = TranscripomeConvertTask()
+        transcriptome_trim_task = TranscriptomeTrimTask(storage_paths=storage_paths)
+
+        pipeline_task = PipelineTask(genome_download_task, transcriptome_download_task, transcriptome_convert_task, transcriptome_trim_task,  transcriptome_align_task)
         pipeline_gateway = PipelineGateway(
             pipeline_repository=pipeline_repository, pipeline_task=pipeline_task
         )
